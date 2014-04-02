874e5b3f4c1bae35c61d8887d334adac29a4cbb7
skinod <wolaws@gmail.com>
2014-04-02 13:36:27 +0430

    Add compatibility for phpass

--- admin/sources/loginauth/joomla/auth.php
+++ admin/sources/loginauth/joomla/auth.php
@@ -128,14 +128,29 @@ class login_joomla extends login_core implements interface_login
 		$html_entities		= array( "&#33;", "&#036;", "&#092;" );
 		$replacement_char	= array( "!", "$", "\\" );
 		$password 			= str_replace( $html_entities, $replacement_char, $password );
+		$hash 				= $joomla_member['password'];
 		
-		$parts	= explode(':', $joomla_member['password']);
-		$crypt	= $parts[0];
-		$salt	= @$parts[1];
-		$testcrypt = $this->_getCryptedPassword($password, $salt, $this->jom_conf['sodjom_hashtype']);
-		
-		
-		if ($crypt != $testcrypt) {
+		// If we are using phpass
+		if (strpos($hash, '$P$') === 0)
+		{
+			require_once(dirname(__FILE__) . "/phpass/PasswordHash.php");
+			// Use PHPass's portable hashes with a cost of 10.
+			$phpass = new PasswordHash(10, true);
+
+			$match = $phpass->CheckPassword($password, $hash);
+		}
+		else
+		{
+			// Check the password
+			$parts = explode(':', $hash);
+			$crypt = $parts[0];
+			$salt  = @$parts[1]; 
+			$testcrypt = md5($password . $salt) . ($salt ? ':' . $salt : '');
+
+			$match = $this->timingSafeCompare($hash, $testcrypt);
+		}
+
+		if ($match !== TRUE) {
 			$this->return_code = 'WRONG_AUTH';
 			return false;
 		}
@@ -162,209 +177,8 @@ class login_joomla extends login_core implements interface_login
 		
 		return true;
 		
-	}
-	
-	//this method is from joomla
-	private function _getCryptedPassword($plaintext, $salt = '', $encryption = 'md5-hex') {
-		$encryption = $encryption?$encryption:'md5-hex';
-		$salt = $this->_getSalt($encryption, $salt, $plaintext);
-
-		switch ($encryption)
-		{
-			case 'plain':
-				return $plaintext;
-
-			case 'sha':
-				$encrypted = base64_encode(mhash(MHASH_SHA1, $plaintext));
-				return $encrypted;
-
-			case 'crypt':
-			case 'crypt-des':
-			case 'crypt-md5':
-			case 'crypt-blowfish':
-				return crypt($plaintext, $salt);
-
-			case 'md5-base64':
-				$encrypted = base64_encode(mhash(MHASH_MD5, $plaintext));
-				return $encrypted;
-
-			case 'ssha':
-				$encrypted = base64_encode(mhash(MHASH_SHA1, $plaintext . $salt) . $salt);
-				return $encrypted;
-
-			case 'smd5':
-				$encrypted = base64_encode(mhash(MHASH_MD5, $plaintext . $salt) . $salt);
-				return $encrypted;
-
-			case 'aprmd5':
-				$length = strlen($plaintext);
-				$context = $plaintext . '$apr1$' . $salt;
-				$binary = $this->_bin(md5($plaintext . $salt . $plaintext));
-
-				for ($i = $length; $i > 0; $i -= 16)
-				{
-					$context .= substr($binary, 0, ($i > 16 ? 16 : $i));
-				}
-				for ($i = $length; $i > 0; $i >>= 1)
-				{
-					$context .= ($i & 1) ? chr(0) : $plaintext[0];
-				}
-
-				$binary = $this->_bin(md5($context));
-
-				for ($i = 0; $i < 1000; $i++)
-				{
-					$new = ($i & 1) ? $plaintext : substr($binary, 0, 16);
-					if ($i % 3)
-					{
-						$new .= $salt;
-					}
-					if ($i % 7)
-					{
-						$new .= $plaintext;
-					}
-					$new .= ($i & 1) ? substr($binary, 0, 16) : $plaintext;
-					$binary = $this->_bin(md5($new));
-				}
-
-				$p = array();
-				for ($i = 0; $i < 5; $i++)
-				{
-					$k = $i + 6;
-					$j = $i + 12;
-					if ($j == 16)
-					{
-						$j = 5;
-					}
-					$p[] = $this->_toAPRMD5((ord($binary[$i]) << 16) | (ord($binary[$k]) << 8) | (ord($binary[$j])), 5);
-				}
-
-				return '$apr1$' . $salt . '$' . implode('', $p) . $this->_toAPRMD5(ord($binary[11]), 3);
-
-			case 'md5-hex':
-			default:
-				$encrypted = ($salt) ? md5($plaintext . $salt) : md5($plaintext);
-				return $encrypted;
-		}
-	}
-	
-	//this method is from joomla
-	private function _bin($hex) {
-		$bin = '';
-		$length = strlen($hex);
-		for ($i = 0; $i < $length; $i += 2)
-		{
-			$tmp = sscanf(substr($hex, $i, 2), '%x');
-			$bin .= chr(array_shift($tmp));
-		}
-		return $bin;
-	}
-	
-	//this method is from joomla
-	private function _getSalt($encryption = 'md5-hex', $seed = '', $plaintext = '') {
-		// Encrypt the password.
-		switch ($encryption)
-		{
-			case 'crypt':
-			case 'crypt-des':
-				if ($seed)
-				{
-					return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 2);
-				}
-				else
-				{
-					return substr(md5(mt_rand()), 0, 2);
-				}
-				break;
-
-			case 'crypt-md5':
-				if ($seed)
-				{
-					return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 12);
-				}
-				else
-				{
-					return '$1$' . substr(md5(mt_rand()), 0, 8) . '$';
-				}
-				break;
-
-			case 'crypt-blowfish':
-				if ($seed)
-				{
-					return substr(preg_replace('|^{crypt}|i', '', $seed), 0, 16);
-				}
-				else
-				{
-					return '$2$' . substr(md5(mt_rand()), 0, 12) . '$';
-				}
-				break;
-
-			case 'ssha':
-				if ($seed)
-				{
-					return substr(preg_replace('|^{SSHA}|', '', $seed), -20);
-				}
-				else
-				{
-					return mhash_keygen_s2k(MHASH_SHA1, $plaintext, substr(pack('h*', md5(mt_rand())), 0, 8), 4);
-				}
-				break;
-
-			case 'smd5':
-				if ($seed)
-				{
-					return substr(preg_replace('|^{SMD5}|', '', $seed), -16);
-				}
-				else
-				{
-					return mhash_keygen_s2k(MHASH_MD5, $plaintext, substr(pack('h*', md5(mt_rand())), 0, 8), 4);
-				}
-				break;
-
-			case 'aprmd5': /* 64 characters that are valid for APRMD5 passwords. */
-				$APRMD5 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-				if ($seed)
-				{
-					return substr(preg_replace('/^\$apr1\$(.{8}).*/', '\\1', $seed), 0, 8);
-				}
-				else
-				{
-					$salt = '';
-					for ($i = 0; $i < 8; $i++)
-					{
-						$salt .= $APRMD5{rand(0, 63)};
-					}
-					return $salt;
-				}
-				break;
-
-			default:
-				$salt = '';
-				if ($seed)
-				{
-					$salt = $seed;
-				}
-				return $salt;
-				break;
-		}
-	}
-	
-	//this method is from joomla
-	protected function _toAPRMD5($value, $count) {
-		/* 64 characters that are valid for APRMD5 passwords. */
-		$APRMD5 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-
-		$aprmd5 = '';
-		$count = abs($count);
-		while (--$count)
-		{
-			$aprmd5 .= $APRMD5[$value & 0x3f];
-			$value >>= 6;
-		}
-		return $aprmd5;
-	}
-	
+	}	
+		
 	/**
 	 * Load a member
 	 *
@@ -381,4 +195,29 @@ class login_joomla extends login_core implements interface_login
 			$this->member_data = IPSMember::load( $member['member_id'], 'extendedProfile,groups' );
 		}
 	}
+
+	// this method is from joomla
+	public static function timingSafeCompare($known, $unknown)
+	{
+		// Prevent issues if string length is 0
+		$known .= chr(0);
+		$unknown .= chr(0);
+
+		$knownLength = strlen($known);
+		$unknownLength = strlen($unknown);
+
+		// Set the result to the difference between the lengths
+		$result = $knownLength - $unknownLength;
+
+		// Note that we ALWAYS iterate over the user-supplied length to prevent leaking length info.
+		for ($i = 0; $i < $unknownLength; $i++)
+		{
+			// Using % here is a trick to prevent notices. It's safe, since if the lengths are different, $result is already non-0
+			$result |= (ord($known[$i % $knownLength]) ^ ord($unknown[$i]));
+		}
+
+		// They are only identical strings if $result is exactly 0...
+		return $result === 0;
+	}
+
 }
